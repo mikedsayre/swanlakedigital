fdf00749f0758027fbf6c5274081bbeb8905070e
Mike Sayre <sayremd@gmail.com>
2026-01-06 15:47:38 -0500

    Update image paths in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const IMAGES = {
-  SWAN_LAKE_FAVICON: 'images/swan-lake-digital-mascot-favicon-ai-developer-builder-64x.png',
-  SWAN_LAKE_MASCOT_512: 'images/swan-lake-digital-mascot-512x.png',
-  PROFILE_HEADSHOT: 'images/mikedsayre-profile.png',
-  SLANGBOT_MASCOT_PLACEHOLDER: 'images/slangbot-mascot-placeholder.png',
+  SWAN_LAKE_FAVICON: '/images/swan-lake-digital-mascot-favicon-ai-developer-builder-64x.png',
+  SWAN_LAKE_MASCOT_512: '/images/swan-lake-digital-mascot-512x.png',
+  PROFILE_HEADSHOT: '/images/mikedsayre-profile.png',
+  SLANGBOT_MASCOT_PLACEHOLDER: '/images/slangbot-app-512px.png',
 };
