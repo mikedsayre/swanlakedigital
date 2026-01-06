8409d310a8b88aeb78416358fc83ee6e41b531b9
Mike Sayre <sayremd@gmail.com>
2026-01-06 15:54:21 -0500

    Fix placeholder image filename for Slangbot mascot

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const IMAGES = {
   SWAN_LAKE_FAVICON: '/images/swan-lake-digital-mascot-favicon-ai-developer-builder-64x.png',
   SWAN_LAKE_MASCOT_512: '/images/swan-lake-digital-mascot-512x.png',
   PROFILE_HEADSHOT: '/images/mikedsayre-profile.png',
-  SLANGBOT_MASCOT_PLACEHOLDER: '/images/slangbot-app-512px.png',
+  SLANGBOT_MASCOT_PLACEHOLDER: '/images/slangbot-app-512x.png',
 };
