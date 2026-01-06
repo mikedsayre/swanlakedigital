fc03e50897f15f6b4d38c726b844612f9267c298
Mike Sayre <sayremd@gmail.com>
2026-01-06 15:26:32 -0500

    Update image paths to include leading slash

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 export const IMAGES = {
-  SWAN_LAKE_FAVICON: 'images/swan-lake-digital-mascot-favicon-ai-developer-builder-64x.png',
-  SWAN_LAKE_MASCOT_512: 'images/swan-lake-digital-mascot-512x.png',
-  PROFILE_HEADSHOT: 'images/mikedsayre-profile.png',
-  SLANGBOT_MASCOT_PLACEHOLDER: 'images/slangbot-app-512x.png',
+  SWAN_LAKE_FAVICON: '/images/swan-lake-digital-mascot-favicon-ai-developer-builder-64x.png',
+  SWAN_LAKE_MASCOT_512: '/images/swan-lake-digital-mascot-512x.png',
+  PROFILE_HEADSHOT: '/images/mikedsayre-profile.png',
+  SLANGBOT_MASCOT_PLACEHOLDER: '/images/slangbot-app-512x.png',
 };
