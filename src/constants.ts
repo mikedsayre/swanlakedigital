d4d5a92f143dc9de88af3080fcb8ebe47c423869
Mike Sayre <sayremd@gmail.com>
2026-01-10 16:18:43 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ export const IMAGES = {
   SWAN_LAKE_MASCOT_512: '/images/swan-lake-digital-mascot-512x.png',
   PROFILE_HEADSHOT: '/images/mikedsayre-profile.png',
   SLANGBOT_MASCOT_PLACEHOLDER: '/images/slangbot-app-512x.png',
-  RESUME_STUDIO_LOGO: '/images/resume-studio-logo.png',
-  JOB_PROFILE_LAB_LOGO: '/images/job-profile-lab-logo.png',
+  RESUME_STUDIO_LOGO: '/images/resume-studio-mascot-256px.png',
+  JOB_PROFILE_LAB_LOGO: '/images/job-lab-mascot-specs-256px.png',
 };
